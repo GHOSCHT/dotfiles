73a136a5bc134f5d13d458dce33ef6792f9447a2
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2023-04-01 17:47:50 +0200

    Add nanokontroller config

---
+++ .config/nanoKontroller/audio2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pactl set-default-sink alsa_output.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.pro-output-0
+pw-link --disconnect LibreWolfSink:monitor_FL alsa_output.pci-0000_0c_00.4.analog-stereo:playback_FL
+pw-link --disconnect LibreWolfSink:monitor_FR alsa_output.pci-0000_0c_00.4.analog-stereo:playback_FR
+pw-link LibreWolfSink:monitor_FL alsa_output.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.pro-output-0:playback_AUX0
+pw-link LibreWolfSink:monitor_FR alsa_output.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.pro-output-0:playback_AUX1
+
+# pw-link LibreWolfSink:monitor_FL easyeffects_sink:playback_FL
+# pw-link LibreWolfSink:monitor_FR easyeffects_sink:playback_FR
