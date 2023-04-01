bfe54d89b82b3aa8ca4dfadefafd2b6c0112b8ef
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2023-04-01 17:43:34 +0200

    Add awesomewm config

---
+++ .config/awesome/autostart.lua
@@ -0,0 +1,28 @@
+local autostart = {}
+
+function autostart.exec(awful)
+    awful.spawn.with_shell("picom --experimental-backends --backend glx --xrender-sync-fence")
+    -- awful.spawn.with_shell("nitrogen --restore")
+    awful.spawn.with_shell("redshift -x &&redshift -O 3500")
+    awful.spawn.with_shell("/usr/lib/polkit-kde-authentication-agent-1")
+    awful.spawn.with_shell("flatpak run com.discordapp.Discord --start-minimized")
+    awful.spawn.with_shell("playerctld daemon")
+    awful.spawn.with_shell("xbindkeys")
+    -- awful.spawn.with_shell("easyeffects --gapplication-service")
+    awful.spawn.with_shell("$HOME/.config/nanoKontroller/initSink.sh")
+    awful.spawn.with_shell("$HOME/.config/nanoKontroller/audio1.sh")
+    awful.spawn.with_shell(
+        "python3 $HOME/.local/bin/nanoKontroller --config $HOME/.config/nanoKontroller/nanoKontroller.ini")
+    -- awful.spawn.with_shell("flatpak run com.spotify.Client")
+    -- awful.spawn.with_shell("/usr/bin/emacs --daemon")
+    awful.spawn.with_shell("/home/ghoscht/.local/bin/razer-cli --dpi 400")
+    awful.spawn.with_shell("electron-mail")
+    awful.spawn.with_shell("clipmenud")
+    awful.spawn.with_shell("nextcloud --background")
+    awful.spawn.with_shell("openrgb --server --profile default")
+    -- awful.spawn.with_shell("autocutsel -fork -selection CLIPBOARD")
+    -- awful.spawn.with_shell("autocutsel -fork -selection PRIMARY")
+    awful.spawn.with_shell("autocutsel")
+end
+
+return autostart
---
+++ .config/awesome/theme.lua
@@ -0,0 +1,95 @@
+--[[
+
+     Powerarrow Dark Awesome WM theme
+     github.com/lcpz
+
+--]]
+
+local gears = require("gears")
+local lain  = require("lain")
+local awful = require("awful")
+local wibox = require("wibox")
+local dpi   = require("beautiful.xresources").apply_dpi
+local colors = require("colors")
+local os = os
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
+
+local theme                                     = {}
+theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/"
+theme.wallpaper_dir                             = theme.dir .. "/wallpapers/"
+theme.wallpaper                                 = theme.wallpaper_dir .. "/robot.jpg"
+theme.font                                      = "JetBrainsMono Nerd Font 9"
+theme.fg_normal                                 = colors.text
+theme.fg_focus                                  = colors.teal
+theme.fg_urgent                                 = "#CC9393"
+theme.bg_normal                                 = colors.base
+theme.bg_focus                                  = colors.crust
+theme.bg_urgent                                 = "#1e1e2e"
+theme.border_width                              = dpi(1)
+theme.border_normal                             = colors.surface0
+theme.border_focus                              = colors.teal
+theme.border_marked                             = "#CC9393"
+theme.tasklist_bg_focus                         = colors.base
+theme.titlebar_bg_focus                         = theme.bg_focus
+theme.titlebar_bg_normal                        = theme.bg_normal
+theme.titlebar_fg_focus                         = theme.fg_focus
+theme.menu_height                               = dpi(16)
+theme.menu_width                                = dpi(140)
+theme.systray_icon_spacing                      = 2
+theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
+theme.taglist_squares_sel                       = theme.dir .. "/icons/square_sel.png"
+theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
+theme.layout_tile                               = theme.dir .. "/icons/tile.png"
+theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
+theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
+theme.layout_tiletop                            = theme.dir .. "/icons/tiletop.png"
+theme.layout_fairv                              = theme.dir .. "/icons/fairv.png"
+theme.layout_fairh                              = theme.dir .. "/icons/fairh.png"
+theme.layout_spiral                             = theme.dir .. "/icons/spiral.png"
+theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.png"
+theme.layout_max                                = theme.dir .. "/icons/max.png"
+theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
+theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
+theme.layout_floating                           = theme.dir .. "/icons/floating.png"
+theme.widget_ac                                 = theme.dir .. "/icons/ac.png"
+theme.widget_battery                            = theme.dir .. "/icons/battery.png"
+theme.widget_battery_low                        = theme.dir .. "/icons/battery_low.png"
+theme.widget_battery_empty                      = theme.dir .. "/icons/battery_empty.png"
+theme.widget_mem                                = theme.dir .. "/icons/custom/mem.png"
+theme.widget_clock                              = theme.dir .. "/icons/custom/clock.png"
+theme.widget_calendar                           = theme.dir .. "/icons/custom/calendar.png"
+theme.widget_cpu                                = theme.dir .. "/icons/custom/chip.png"
+theme.widget_temp                               = theme.dir .. "/icons/temp.png"
+theme.widget_net                                = theme.dir .. "/icons/net.png"
+theme.widget_hdd                                = theme.dir .. "/icons/hdd.png"
+theme.widget_music                              = theme.dir .. "/icons/note.png"
+theme.widget_music_on                           = theme.dir .. "/icons/note_on.png"
+theme.widget_vol                                = theme.dir .. "/icons/vol.png"
+theme.widget_vol_low                            = theme.dir .. "/icons/vol_low.png"
+theme.widget_vol_no                             = theme.dir .. "/icons/vol_no.png"
+theme.widget_vol_mute                           = theme.dir .. "/icons/vol_mute.png"
+theme.widget_mail                               = theme.dir .. "/icons/mail.png"
+theme.widget_mail_on                            = theme.dir .. "/icons/mail_on.png"
+theme.tasklist_plain_task_name                  = true
+theme.tasklist_disable_icon                     = true
+theme.useless_gap                               = dpi(7)
+theme.titlebar_close_button_focus               = theme.dir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal              = theme.dir .. "/icons/titlebar/close_normal.png"
+theme.titlebar_ontop_button_focus_active        = theme.dir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active       = theme.dir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.dir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.dir .. "/icons/titlebar/ontop_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active       = theme.dir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active      = theme.dir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.dir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.dir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_floating_button_focus_active     = theme.dir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active    = theme.dir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive   = theme.dir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive  = theme.dir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active    = theme.dir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = theme.dir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
+
+return theme
